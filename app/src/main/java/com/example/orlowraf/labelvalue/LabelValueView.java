4fbc479d94efe730ceaa6fb1d4855937fbd529ae
Rafał Orłowski <rufaraday@gmail.com>
2015-12-11 11:16:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/orlowraf/labelvalue/LabelValueView.java
@@ -0,0 +1,56 @@
+package com.example.orlowraf.labelvalue;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by orlowraf on 2015-12-11.
+ */
+class LabelValueView extends LinearLayout {
+    private final String labelContent;
+    private final String valueContent;
+    private TextView labelView;
+    private TextView valueView;
+
+    public LabelValueView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray styledAttributes = context
+                .getTheme().obtainStyledAttributes(attrs, R.styleable.LabelValueView, 0, 0);
+        labelContent = styledAttributes.getString(R.styleable.LabelValueView_labelText);
+        valueContent = styledAttributes.getString(R.styleable.LabelValueView_valueText);
+        styledAttributes.recycle();
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.label_value, this, true);
+        labelView = (TextView) findViewById(R.id.label);
+        valueView = (TextView) findViewById(R.id.value);
+        labelView.setText(labelContent);
+        valueView.setText(valueContent);
+    }
+
+    public void setLayout(String label, String value) {
+        labelView.setText(label);
+        valueView.setText(value);
+    }
+
+    public void setLayout(LabelValueModel model) {
+        labelView.setText(model.labelContent);
+        valueView.setText(model.valueContent);
+    }
+
+    public static class LabelValueModel {
+        private final String labelContent;
+        private final String valueContent;
+
+        public LabelValueModel(String labelContent, String valueContent) {
+            this.labelContent = labelContent;
+            this.valueContent = valueContent;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/orlowraf/labelvalue/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.orlowraf.labelvalue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+public class MainActivity extends AppCompatActivity {
+    private LabelValueView labelValueView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        labelValueView = (LabelValueView) findViewById(R.id.label_value_view);
+        //labelValueView.setLayout("Hello", "world");
+    }
+}
