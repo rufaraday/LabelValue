c7a0c2e68c36bebf9e0f73be9a96d38c3c160f10
Rafał Orłowski <rufaraday@gmail.com>
2015-12-11 16:42:30 +0100

    Better bitmap scale attempt

--- app/src/main/java/com/example/orlowraf/labelvalue/AvatarView.java
+++ app/src/main/java/com/example/orlowraf/labelvalue/AvatarView.java
@@ -10,7 +10,9 @@ import android.graphics.Paint;
 import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -19,12 +21,17 @@ import android.view.View;
 public class AvatarView extends View {
 
     private Paint paint;
+    private Paint stroke;
     private Shader shader;
+    private boolean isPressed;
 
     public AvatarView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setBackgroundColor(Color.RED);
         paint = new Paint();
+        stroke = new Paint();
+        stroke.setColor(Color.MAGENTA);
+        stroke.setStyle(Paint.Style.STROKE);
+        stroke.setStrokeWidth(7);
     }
 
     @Override
@@ -42,12 +49,36 @@ public class AvatarView extends View {
         paint.setAntiAlias(true);
         float halfSize = canvas.getHeight()/2;
         canvas.drawCircle(halfSize, halfSize, halfSize, paint);
+        if (isPressed) {
+            canvas.drawCircle(halfSize, halfSize, halfSize-3, stroke);
+        }
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.been);
+        int bitmapW = bitmap.getWidth();
+        int bitmapH = bitmap.getHeight();
+        float factor = Math.min(bitmapH, bitmapW) / w;
+        Log.d("RO", "bitmapW=" + bitmapW + "bitmapH" + bitmapH + " " + Math.round(bitmapW*factor*w) + " " + Math.round(bitmapH*factor*w));
         shader = new BitmapShader(Bitmap.createScaledBitmap(bitmap, w, h, true),
                 BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                isPressed = true;
+                invalidate();
+                return true;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                isPressed = false;
+                invalidate();
+                return true;
+            default:
+                return super.onTouchEvent(event);
+        }
     }
 }
